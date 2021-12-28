204de50feb203b6052778b7dd971af4d1149275d
Eoin O'Brien <eoin@tracworx.ai>
2021-12-28 00:47:02 +0000

    Convert index.ts to barrel format

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { DataloaderModule } from './dataloader.module';
-export { DataloaderProvider } from './decorators/dataloader-provider.decorator';
-export { Loader } from './decorators/loader.decorator';
+export * from './dataloader.module';
+export * from './decorators/dataloader-provider.decorator';
+export * from './decorators/loader.decorator';
+export * from './interceptors/dataloader.interceptor';
+export * from './interfaces/dataloader-factory.interface';
+export * from './services/dataloader-discovery.service';
